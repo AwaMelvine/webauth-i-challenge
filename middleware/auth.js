28547749ad3ece6bc0c360e94fb388ae2f793e25
Awa Melvine <melvineawa@gmail.com>
2019-07-23 15:18:59 +0100

    Refactor the restricted middleware to use session

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,10 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
     async restricted(req, res, next) {
-        const { username, password } = req.headers;
-
-        if (!username || !password) {
-            return res.status(401).json({ message: 'You must login first' });
-        }
-
-        const user = await User.findByUsername(username);
-        if (user && bcrypt.compareSync(password, user.password)) {
-            req.user = user;
+        if (req.session.user) {
             next();
         } else {
-            res.status(401).json({ message: "Unauthorized!" });
+            return res.status(401).json({ message: 'You must login first' });
         }
     }
 }
