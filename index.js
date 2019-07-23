ae2aecefe02af7bc7286ecfb947cbfb15a25be61
Awa Melvine <melvineawa@gmail.com>
2019-07-23 13:41:56 +0100

    Enforce unique constraint on username

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get('*', (req, res) => {
     res.status(404).json({ message: 'Page Not Found' });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 app.listen(port, console.log(`Server running on localhost:${port}`));
--- routes/auth.js
+++ routes/auth.js
@@ -7,8 +7,15 @@ const router = new Router();
 router.post('/register', async (req, res) => {
     try {
         let user = req.body;
-        const newUser = await User.add(user);
 
+        user.password = bcrypt.hashSync(user.password, 12);
+        const prevUser = await User.findByUsername(user.username);
+
+        if (prevUser) {
+            return res.status(400).json({ error: 'User already exists' })
+        }
+
+        const newUser = await User.add(user);
         res.status(201).json({ data: newUser });
     } catch (error) {
         res.status(500).json({ error: 'Failed to register' });
